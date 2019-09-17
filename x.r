10135565da834405e4e0818c6263440004244816
Drew Schmidt <wrathematics@gmail.com>
2019-09-17 08:43:44 -0400

    better naming in example

--- x.r
+++ x.r
@@ -1,15 +1,15 @@
 suppressMessages(library(cop))
 
-mult_plus_one = function(a, b) a+b #qr.R(qr(rbind(a, b)))
+add = function(a, b) a+b #qr.R(qr(rbind(a, b)))
 
 x = matrix(1:4, 2) + 10*comm.rank()
 
-out = cop_allreduce(x, fun=mult_plus_one, commutative=TRUE)
+out = cop_allreduce(x, fun=add, commutative=TRUE)
 
 mpi_cat("\n", quiet=TRUE)
 
 mpi_cat("function:\n", quiet=TRUE)
-mpi_print(mult_plus_one)
+mpi_print(add)
 
 ar=TRUE
 mpi_cat("\ninput:\n", quiet=TRUE)
