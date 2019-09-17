d0ff7de1aa95092780e42b05773600e0bb4fb95a
Drew Schmidt <wrathematics@gmail.com>
2019-09-17 08:43:24 -0400

    comments

--- src/cop_allreduce.c
+++ src/cop_allreduce.c
@@ -68,18 +68,19 @@ size_t _len;
 
 
 
-static void mat_op_eval_R(double *a, double *b, int *len, MPI_Datatype *dtype)
+static void custom_op_matrix(double *a, double *b, int *len, MPI_Datatype *dtype)
 {
   (void)a;
+  (void)b;
   (void)len;
   (void)dtype;
+  
+  for (int i=0; i<4; i++) b[i] += a[i];
   
   // SEXP lhs;
   // PROTECT(lhs = eval(R_fcall, R_env));
   // memcpy(b, REAL(lhs), _len);
-  // 
   // UNPROTECT(1);
-  for (int i=0; i<4; i++) b[i] += a[i];
 }
 
 
@@ -88,6 +89,8 @@ SEXP cop_allreduce_mat_userop(SEXP send_data, SEXP R_comm, SEXP fun,
   SEXP commutative, SEXP env, SEXP root)
 {
   MPI_Comm comm = get_mpi_comm_from_Robj(R_comm);
+  
+  // data setup
   const int m = nrows(send_data);
   const int n = ncols(send_data);
   _len = (size_t) m*n*sizeof(double);
@@ -95,25 +98,26 @@ SEXP cop_allreduce_mat_userop(SEXP send_data, SEXP R_comm, SEXP fun,
   SEXP recv_data;
   PROTECT(recv_data = allocMatrix(REALSXP, m, n));
   
-  PROTECT(R_env);
-  R_env = env;
-  PROTECT(R_fcall);
-  R_fcall = lang3(fun, send_data, recv_data);
+  PROTECT(R_env = env);
+  PROTECT(R_fcall = lang3(fun, send_data, recv_data));
   
+  // custom data type
   MPI_Datatype mat_type;
   MPI_Type_contiguous(m*n, MPI_DOUBLE, &mat_type);
   MPI_Type_commit(&mat_type);
   
+  // custom op + reduce
   MPI_Op op;
-  MPI_Op_create((MPI_User_function*) mat_op_eval_R, LOGICAL(commutative)[0], &op);
+  MPI_Op_create((MPI_User_function*) custom_op_matrix, LOGICAL(commutative)[0], &op);
   int ret;
   if (INTEGER(root)[0] == REDUCE_TO_ALL)
     ret = MPI_Allreduce(REAL(send_data), REAL(recv_data), 1, mat_type, op, comm);
   else
     ret = MPI_Reduce(REAL(send_data), REAL(recv_data), 1, mat_type, op, INTEGER(root)[0], comm);
   check_MPI_ret(ret);
-  MPI_Op_free(&op);
   
+  // cleanup and return
+  MPI_Op_free(&op);
   MPI_Type_free(&mat_type);
   
   UNPROTECT(3);
