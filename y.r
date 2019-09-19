d802078cf1f50f30c736070f238dc08e64f77f9f
Drew Schmidt <wrathematics@gmail.com>
2019-09-19 07:08:46 -0400

    update test scripts

--- y.r
+++ y.r
@@ -2,19 +2,21 @@ suppressMessages(library(cop))
 
 x = matrix(1:4, 2) + 10*comm.rank()
 
-out = qr_allreduce(x)
-
 mpi_cat("\n", quiet=TRUE)
 
 mpi_cat("function:\n", quiet=TRUE)
 mpi_print("qr_allreduce")
 
-ar=TRUE
+ar=FALSE
 mpi_cat("\ninput:\n", quiet=TRUE)
 mpi_print(x, all.rank=ar)
 
+out = qr_allreduce(x)
 mpi_cat("\noutput:\n", quiet=TRUE)
-comm.print(out, all.rank=T)
-# mpi_print(out, all.rank=ar)
+mpi_print(out, all.rank=ar)
+
+out = qr_allreduce(x, type="float")
+mpi_cat("\noutput:\n", quiet=TRUE)
+mpi_print(out, all.rank=ar)
 
 finalize()
