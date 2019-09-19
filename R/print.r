19f20fa1ec1c60287fd0dee5d1dd3361b6856171
Drew Schmidt <wrathematics@gmail.com>
2019-09-19 07:41:58 -0400

    add comm arg to mpi printer; add mpi_print docs

--- R/print.r
+++ R/print.r
@@ -6,14 +6,14 @@ rankcat = function(rank, quiet)
  invisible()
 }
 
-printer = function(printfun, msg, quiet=FALSE, all.rank=FALSE)
+printer = function(printfun, x, quiet=FALSE, all.rank=FALSE, comm=0L)
 {
-  myrank = pbdMPI::comm.rank()
+  myrank = pbdMPI::comm.rank(comm=comm)
   
   if (myrank == 0)
   {
     rankcat(myrank, quiet)
-    printfun(msg)
+    printfun(x)
   }
   
   if (all.rank)
@@ -22,35 +22,75 @@ printer = function(printfun, msg, quiet=FALSE, all.rank=FALSE)
     {
       if (myrank == 0)
       {
-        rankmsg_len = pbdMPI::spmd.recv.integer(integer(1), rank.source=rank)
-        rankmsg = pbdMPI::recv(character(rankmsg_len), rank.source=rank)
+        rankmsg_len = pbdMPI::spmd.recv.integer(integer(1), rank.source=rank, comm=comm)
+        rankmsg = pbdMPI::recv(character(rankmsg_len), rank.source=rank, comm=comm)
         rankcat(rank, quiet)
         cat(rankmsg)
       }
       else
       {
-        cmsg = paste(capture.output(printfun(msg)), "\n", collapse="")
-        pbdMPI::spmd.send.integer(nchar(cmsg), rank.dest=0)
-        pbdMPI::send(cmsg, rank.dest=0)
+        cx = paste(capture.output(printfun(x)), "\n", collapse="")
+        pbdMPI::spmd.send.integer(nchar(cx), rank.dest=0, comm=comm)
+        pbdMPI::send(cx, rank.dest=0, comm=comm)
       }
     }
   }
   
-  pbdMPI::barrier()
+  pbdMPI::barrier(comm=comm)
+  invisible()
 }
 
 
 
+#' MPI Printer
+#' 
+#' Orderly printing of data across MPI ranks. Similar to
+#' \code{pbdMPI::comm.print()}.
+#' 
+#' @details
+#' All printing is done on rank 0 to ensure printing occurs in rank order. This
+#' requires communication if \code{all.rank=TRUE}.
+#' 
+#' @param x
+#' The input to be printed.
+#' @param quiet
+#' Should ranks print their rank number before printing the input?
+#' @param all.rank
+#' Should all ranks print or only rank 0?
+#' @param comm
+#' MPI communicator number.
+#' 
+#' @return
+#' Invisibly returns \code{NULL}.
+#' 
+#' @examples
+#' \dontrun{
+#' suppressMessages(library(cop))
+#' 
+#' x = matrix(1:4, 2) + 10*comm.rank()
+#' mpi_print(x)
+#' 
+#' finalize()
+#' }
+#' 
+#' @name mpi_print
+#' @rdname mpi_print
+NULL
+
+
+
+#' @rdname mpi_print
 #' @export
-mpi_cat = function(msg, quiet=FALSE, all.rank=FALSE)
+mpi_cat = function(x, quiet=FALSE, all.rank=FALSE, comm=0L)
 {
-  printer(cat, msg, quiet, all.rank)
+  printer(cat, x=x, quiet=quiet, all.rank=all.rank, comm=comm)
 }
 
 
 
+#' @rdname mpi_print
 #' @export
-mpi_print = function(msg, quiet=FALSE, all.rank=FALSE)
+mpi_print = function(x, quiet=FALSE, all.rank=FALSE, comm=0L)
 {
-  printer(print, msg, quiet, all.rank)
+  printer(print, x=x, quiet=quiet, all.rank=all.rank, comm=comm)
 }
