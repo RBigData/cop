5793d87ad9ba2276eb385bea0dec939d724687da
Drew Schmidt <wrathematics@gmail.com>
2019-09-23 05:44:16 -0400

    update cop_allreduce example

--- inst/examples/cop_allreduce.r
+++ inst/examples/cop_allreduce.r
@@ -1,19 +1,24 @@
 suppressMessages(library(cop))
 
-add = function(a, b) a+b #qr.R(qr(rbind(a, b)))
-
 x = matrix(1:4, 2) + 10*comm.rank()
 
+add = function(a, b) a+b
 out = cop_allreduce(x, fun=add, commutative=TRUE)
-
-mpi_cat("\n", quiet=TRUE)
-
-mpi_cat("function:\n", quiet=TRUE)
-mpi_print(add)
 
 mpi_cat("\ninput:\n", quiet=TRUE)
 mpi_print(x, all.rank=TRUE)
 
+mpi_cat("\nfunction:\n", quiet=TRUE)
+mpi_print(add)
+mpi_cat("\noutput:\n", quiet=TRUE)
+mpi_print(out)
+
+
+R = function(a, b) qr.R(qr(rbind(a, b)))
+out = cop_allreduce(x, fun=R, commutative=TRUE)
+
+mpi_cat("\nfunction:\n", quiet=TRUE)
+mpi_print(R)
 mpi_cat("\noutput:\n", quiet=TRUE)
 mpi_print(out)
 
