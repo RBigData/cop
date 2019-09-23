a8f8bcd5d56afa76d27b03aef62622ca7427d6a5
Drew Schmidt <wrathematics@gmail.com>
2019-09-23 16:45:11 -0400

    update qr validation script

--- inst/validation/qr.r
+++ inst/validation/qr.r
@@ -1,25 +1,36 @@
 suppressMessages(library(cop))
 comm.set.seed(1234, diff=TRUE)
 
+getR = function(x) qr.R(qr(x, LAPACK=TRUE))
+
 m = 100
-n = 4
-x = qr.R(qr(matrix(rnorm(m*n), m, n), LAPACK=TRUE))
+n = 5
+x = getR(matrix(rnorm(m*n), m, n))
 
 R_ar = qr_reduce(x)
+
+op = function(a, b) getR(rbind(a, b))
+R_ar_cop = cop_allreduce(x, op, commutative=TRUE)
+
+if (comm.rank() == 0){
+  test = all.equal(abs(R_ar), abs(R_ar_cop))
+  print(test)
+}
 
 x_g = gather(x)
 if (comm.rank() == 0){
   R_i = x_g[[1]]
   for (i in seq.int(comm.size()-1)+1){
-    R_i = qr.R(qr(rbind(R_i, x_g[[i]]), LAPACK=TRUE))
+    R_i = op(R_i, x_g[[i]])
   }
   test = all.equal(abs(R_ar), abs(R_i))
   print(test)
-  
+
   x_all = do.call(rbind, x_g)
-  R = qr.R(qr(x_all, LAPACK=TRUE))
+  R = getR(x_all)
   test = all.equal(abs(R_ar), abs(R))
   print(test)
 }
+
 
 finalize()
