ccd6a6ee1442419e7b421c06d744b56106fdbe24
Drew Schmidt <wrathematics@gmail.com>
2019-09-23 10:33:13 -0400

    template debug printer

--- src/debug/printer.hpp
+++ src/debug/printer.hpp
@@ -2,14 +2,15 @@
 #define COP_DEBUG_PRINTER_H
 
 
-#include <stdio.h>
+#include <cstdio>
 
-static inline void matprinter(int m, int n, double *x)
+template <typename REAL>
+static inline void matprinter(int m, int n, REAL *x)
 {
   for (int i=0; i<m; i++)
   {
     for (int j=0; j<n; j++)
-      printf("%.2f ", x[i + m*j]);
+      printf("%.2f ", (REAL) x[i + m*j]);
     
     putchar('\n');
   }
