3e88242b7906981cb5fe6bb3eb0b17dd973ba722
wrathematics <wrathematics@gmail.com>
2019-09-24 15:32:14 -0400

    add integer printer

--- src/debug/printer.hpp
+++ src/debug/printer.hpp
@@ -4,6 +4,7 @@
 
 #include <cstdio>
 
+
 template <typename REAL>
 static inline void matprinter(int m, int n, REAL *x)
 {
@@ -11,6 +12,18 @@ static inline void matprinter(int m, int n, REAL *x)
   {
     for (int j=0; j<n; j++)
       printf("%.2f ", (REAL) x[i + m*j]);
+    
+    putchar('\n');
+  }
+  putchar('\n');
+}
+
+static inline void matprinter(int m, int n, int *x)
+{
+  for (int i=0; i<m; i++)
+  {
+    for (int j=0; j<n; j++)
+      printf("%d ", x[i + m*j]);
     
     putchar('\n');
   }
