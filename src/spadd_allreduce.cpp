cd42faba9cd3bbfa9a863a9a4e67590451963980
wrathematics <wrathematics@gmail.com>
2019-09-24 15:35:35 -0400

    add s4 dgCMatrix to eigen sparse matrix converter

--- src/spadd_allreduce.cpp
+++ src/spadd_allreduce.cpp
@@ -33,7 +33,39 @@ void arrcopy(const index_t len, const SRC *src, DST *dst)
 
 
 
+static inline spmat s4_to_spmat(SEXP s4)
+{
+  SEXP dim = GET_SLOT(s4, install("Dim"));
+  const int m = INTEGER(dim)[0];
+  const int n = INTEGER(dim)[1];
+  
+  SEXP s4_X = GET_SLOT(s4, install("x"));
+  const int nnz = LENGTH(s4_X);
+  
+  SEXP s4_I = GET_SLOT(s4, install("i")); // len == nnz
+  
+  SEXP s4_P = GET_SLOT(s4, install("p"));
+  const int P_len = LENGTH(s4_P);
+  
+  spmat s(m, n);
+  s.makeCompressed();
+  s.resizeNonZeros(nnz);
+  
+  arrcopy(nnz, REAL(s4_X), spmat_X(s));
+  arrcopy(nnz, INTEGER(s4_I), spmat_I(s));
+  arrcopy(P_len, INTEGER(s4_P), spmat_P(s));
+  
+  return s;
+}
+
+
+
 extern "C" SEXP cop_allreduce_spmat_add(SEXP send_data_s4, SEXP R_comm)
 {
+  MPI_Comm comm = get_mpi_comm_from_Robj(R_comm);
+  
+  spmat send_data = s4_to_spmat(send_data_s4);
+  std::cout << send_data << std::endl;
+  
   return R_NilValue;
 }
