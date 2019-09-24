372bdc1d1447e2ba0f16154eb0df4bf9843f4bb3
wrathematics <wrathematics@gmail.com>
2019-09-24 15:34:37 -0400

    add some type helpers

--- src/spadd_allreduce.cpp
+++ src/spadd_allreduce.cpp
@@ -5,6 +5,17 @@
 #include <Rdefines.h>
 #include <Rinternals.h>
 
+#include "cxxutils.hpp"
+#include "utils.h"
+
+typedef uint32_t scalar_t;
+typedef int index_t;
+typedef Eigen::SparseMatrix<scalar_t, Eigen::StorageOptions::ColMajor, index_t> spmat;
+
+static inline scalar_t* spmat_X(spmat &s){return (scalar_t*) s.valuePtr();}
+static inline index_t* spmat_I(spmat &s){return (index_t*) s.innerIndexPtr();}
+static inline index_t* spmat_P(spmat &s){return (index_t*) s.outerIndexPtr();}
+
 
 
 extern "C" SEXP cop_allreduce_spmat_add(SEXP send_data_s4, SEXP R_comm)
