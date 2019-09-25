eea85506b4dfea245cb83d609c940e8108ddbec1
Drew Schmidt <wrathematics@gmail.com>
2019-09-25 10:43:58 -0400

    add in MPI shim

--- R/spadd_allreduce.r
+++ R/spadd_allreduce.r
@@ -1,15 +1,22 @@
 #' @useDynLib cop cop_allreduce_spmat_add
+spadd_reducer = function(x, root, comm)
+{
+  comm = as.integer(comm)
+  comm_ptr = pbdMPI::get.mpi.comm.ptr(comm)
+  
+  .Call(cop_allreduce_spmat_add, x, root, comm_ptr)
+}
 
 
 
 #' @export
 spadd_allreduce = function(x, comm=0L)
 {
-  .Call(cop_allreduce_spmat_add, x, comm)
+  spadd_reducer(x=x, root=REDUCE_TO_ALL, comm=comm)
 }
 
 #' @export
-spadd_reduce = function(x, root=0, comm=0L)
+spadd_reduce = function(x, root=0L, comm=0L)
 {
-
+  spadd_reducer(x=x, root=root, comm=comm)
 }
--- src/spadd_allreduce.cpp
+++ src/spadd_allreduce.cpp
@@ -1,14 +1,42 @@
-#include <iostream>
-
 #include "cxxutils.hpp"
+#include "defs.h"
 #include "eigenhelpers.hpp"
 #include "utils.h"
-
 
 using namespace eigenhelpers;
 
 
-extern "C" SEXP cop_allreduce_spmat_add(SEXP send_data_s4, SEXP R_comm)
+static inline int spadd_allreduce(const int root, spmat &send_data, MPI_Comm comm)
+{
+  int mpi_ret;
+  
+  const index_t m = send_data.rows();
+  const index_t n = send_data.cols();
+  scalar_t *tmp = (scalar_t*) malloc(m * sizeof(*tmp));
+  
+  for (index_t j=0; j<n; j++)
+  {
+    sparsecol_to_densecol(j, send_data, tmp);
+    
+    if (root == REDUCE_TO_ALL)
+      mpi_ret = MPI_Allreduce(MPI_IN_PLACE, tmp, m, MPI_SCALAR_T, MPI_SUM, comm);
+    else
+      mpi_ret = MPI_Reduce(MPI_IN_PLACE, tmp, m, MPI_SCALAR_T, MPI_SUM, root, comm);
+    
+    
+    // TODO copy back
+    
+  }
+  
+  
+  free(tmp);
+  
+  return mpi_ret;
+}
+
+
+
+extern "C" SEXP cop_allreduce_spmat_add(SEXP send_data_s4, SEXP root, SEXP R_comm)
 {
   MPI_Comm comm = get_mpi_comm_from_Robj(R_comm);
   
@@ -21,7 +49,8 @@ extern "C" SEXP cop_allreduce_spmat_add(SEXP send_data_s4, SEXP R_comm)
   x.insert(9, 0) = 1;
   x.insert(3, 1) = 1;
   x += send_data;
-  std::cout << x << std::endl;
+  
+  spadd_allreduce(INTEGER(root)[0], x, comm);
   
   SEXP ret;
   PROTECT(ret = spmat_to_s4(send_data));
