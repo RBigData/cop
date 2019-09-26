16729a96bd8889d19d49b049d74092df1b4851d2
Drew Schmidt <wrathematics@gmail.com>
2019-09-26 07:06:31 -0400

    update sparse experiments

--- inst/experiments/sparse/test.cpp
+++ inst/experiments/sparse/test.cpp
@@ -1,54 +1,15 @@
 #include <cstdint>
 #include <iostream>
 
-#include <Eigen/SparseCore>
+#include "../../../src/spvec.hpp"
 
 
 int main()
 {
-  int n = 5;
+  spvec<int, int> v(5);
   
-  Eigen::SparseMatrix<uint32_t, Eigen::StorageOptions::ColMajor, int> x(n, n);
-  x.makeCompressed();
-  x.reserve(4);
-  x.insert(0, 0) = 1;
-  x.insert(2, 4) = 1;
-  
-  Eigen::SparseMatrix<uint32_t, Eigen::StorageOptions::ColMajor, int> y(n, n);
-  y.makeCompressed();
-  y.reserve(4);
-  y.insert(0, 1) = 1;
-  y.insert(2, 4) = 1;
-  
-  x = x+y;
-  std::cout << x << std::endl;
-  
-  // x.resize(rows, cols);
-  // x.makeCompressed();
-  // x.resizeNonZeros(nnz);
-  // memcpy(x.valuePtr(), values, nnz * sizeof(*values));
-  // memcpy(x.outerIndexPtr(), P, P_len * sizeof(*P));
-  // memcpy(x.innerIndexPtr(), I, nnz * sizeof(*I));
-  
-  int nnz = x.nonZeros();
-  uint32_t *X = x.valuePtr();
-  std::cout << "X: ";
-  for (int i=0; i<nnz; i++)
-    std::cout << X[i] << " ";
-  std::cout << std::endl;
-  
-  int *I = x.innerIndexPtr();
-  std::cout << "I: ";
-  for (int i=0; i<nnz; i++)
-    std::cout << I[i] << " ";
-  std::cout << std::endl;
-  
-  int P_len = x.outerSize();
-  int *P = x.outerIndexPtr();
-  std::cout << "P: ";
-  for (int i=0; i<P_len; i++)
-    std::cout << P[i] << " ";
-  std::cout << std::endl;
+  v.insert(3, 1);
+  v.print();
   
   return 0;
 }
