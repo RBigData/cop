7032ab3893294641f0d68516d8a6f49cd6c5897b
wrathematics <wrathematics@gmail.com>
2019-09-26 12:05:00 -0400

    update spvec test

--- inst/experiments/sparse/test.cpp
+++ inst/experiments/sparse/test.cpp
@@ -6,9 +6,23 @@
 
 int main()
 {
-  spvec<int, int> v(5);
+  spvec<int, int> u(5);
+  u.insert(3, 1);
+  u.insert(1, 2);
+  u.insert(2, 3);
+  u.print();
   
+  spvec<int, int> v(5);
+  v.insert(0, 1);
+  v.insert(2, 1);
   v.insert(3, 1);
+  v.insert(4, 5);
+  v.print();
+  
+  u.add(v);
+  u.print();
+  
+  v.zero();
   v.print();
   
   return 0;
