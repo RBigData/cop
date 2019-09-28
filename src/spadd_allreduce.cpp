9aca76688683419edecab5bc6fe919b4f965162b
Drew Schmidt <wrathematics@gmail.com>
2019-09-27 21:26:12 -0400

    drop eigen dep

--- src/spadd_allreduce.cpp
+++ src/spadd_allreduce.cpp
@@ -1,12 +1,10 @@
 #include "cxxutils.hpp"
 #include "defs.h"
-#include "eigenhelpers.hpp"
 #include "utils.h"
 
-using namespace eigenhelpers;
 
-
-static inline int spadd_allreduce(const int root, spmat &send_data, MPI_Comm comm)
+/*
+static inline int spadd_allreduce(const int root, MPI_Comm comm)
 {
   int mpi_ret;
   
@@ -16,7 +14,6 @@ static inline int spadd_allreduce(const int root, spmat &send_data, MPI_Comm com
   
   for (index_t j=0; j<n; j++)
   {
-    sparsecol_to_densecol(j, send_data, tmp);
     
     if (root == REDUCE_TO_ALL)
       mpi_ret = MPI_Allreduce(MPI_IN_PLACE, tmp, m, MPI_SCALAR_T, MPI_SUM, comm);
@@ -33,6 +30,7 @@ static inline int spadd_allreduce(const int root, spmat &send_data, MPI_Comm com
   
   return mpi_ret;
 }
+*/
 
 
 
@@ -40,20 +38,7 @@ extern "C" SEXP cop_allreduce_spmat_add(SEXP send_data_s4, SEXP root, SEXP R_com
 {
   MPI_Comm comm = get_mpi_comm_from_Robj(R_comm);
   
-  spmat send_data = s4_to_spmat(send_data_s4);
+  // TODO
   
-  spmat x(send_data.rows(), send_data.cols());
-  x.makeCompressed();
-  x.reserve(3);
-  x.insert(0, 0) = 1;
-  x.insert(9, 0) = 1;
-  x.insert(3, 1) = 1;
-  x += send_data;
-  
-  spadd_allreduce(INTEGER(root)[0], x, comm);
-  
-  SEXP ret;
-  PROTECT(ret = spmat_to_s4(send_data));
-  UNPROTECT(1);
-  return ret;
+  return R_NilValue;
 }
