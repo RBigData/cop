16e06114d9e0d07a7d10c563ef3b38c890ce189f
wrathematics <wrathematics@gmail.com>
2019-11-05 11:20:58 -0500

    expand supported types

--- R/00-globals.r
+++ R/00-globals.r
@@ -10,11 +10,11 @@ REDUCE_TO_ALL = -1L
 # type globals
 # -----------------------------------------------------------------------------
 
-TYPE_DOUBLE = 1L
-TYPE_FLOAT = 2L
+TYPES_STR_NUMERIC = c("double", "float")
+TYPES_STR_INTEGER = c("int", "int8_t", "int16_t", "int32_t", "int64_t", "uint8_t", "uint16_t", "uint32_t", "uint64_t")
+TYPES_STR = c(TYPES_STR_NUMERIC, TYPES_STR_INTEGER)
 
-TYPES_STR = c("double", "float")
-TYPES_INT = 1:2
+TYPES_INT = 1:length(TYPES_STR)
 names(TYPES_INT) = TYPES_STR
 
 type_int2str = function(type)
--- src/defs.h
+++ src/defs.h
@@ -8,6 +8,15 @@
 
 #define TYPE_DOUBLE 1
 #define TYPE_FLOAT 2
+#define TYPE_INT 3
+#define TYPE_INT8 4
+#define TYPE_INT16 5
+#define TYPE_INT32 6
+#define TYPE_INT64 7
+#define TYPE_UINT8 8
+#define TYPE_UINT16 9
+#define TYPE_UINT32 10
+#define TYPE_UINT64 11
 
 #define MAX(a,b) ((a)<(b)?(b):(a))
 #define MIN(a,b) ((a)<(b)?(a):(b))
